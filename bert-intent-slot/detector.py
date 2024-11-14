d90f51594597e3c55f2d69a6378b3934e1727c6a
HeyanZhuo <76900053+goxtopia@users.noreply.github.com>
2024-11-14 14:27:50 +0800

    Update detector.py

--- bert-intent-slot/detector.py
+++ bert-intent-slot/detector.py
@@ -52,6 +52,7 @@ class JointIntentSlotDetector:
                 results[slot_name] = [slot_value]
             return results
 
+        print(slot_labels)
         for i, slot_label in enumerate(slot_labels):
             if mask[i] == 0:
                 continue
