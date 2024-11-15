e7c3a1d06f2a904853980d4d804477d09629e13c
HeyanZhuo <76900053+goxtopia@users.noreply.github.com>
2024-11-15 14:27:43 +0800

    Update preditc.py

--- bert-intent-slot/preditc.py
+++ bert-intent-slot/preditc.py
@@ -16,7 +16,7 @@ model = JointIntentSlotDetector.from_pretrained(
 )
 start2_time = time.perf_counter()
 all_text = ['定位我现在的位置', "现在几点了", "2013年亚洲冠军联赛恒广州恒大比赛时间。", "帮我查一下赣州到厦门的汽车", "导航到望江西路上去", "把张玉娟的手机号码发送给吴伟", "打电话给xxx", "经XXX的电话号码发给lc"
-            "发信息给盛吉", "将你在哪发送给纲吉", "发信息给老妈说我在吃饭", "我要听稻香", "访问浏览器", "中国制用英文怎么说"]
+            "发信息给盛吉", "将你在哪发送给纲吉", "发信息给老妈说我在吃饭", "我要听稻香", "访问浏览器", "中国制用英文怎么说","把刘荣涛的电话发给王华成"]
 for i in all_text:
     print(model.detect(i))
 end_time = time.perf_counter()
